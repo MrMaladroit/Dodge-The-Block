a3aea981a8f821db4b386f4b20bdd36f0b176935
Jim Beck <jamesbeckjr@gmail.com>
2018-05-05 19:27:54 -0400

    BlockSpawner
    Created BlockSpawner GameObject
    Created BlockSpawner.cs
    Added Block to Prefab folder

---
+++ Dodge-The-Block/Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] spawnPoints;
+    [SerializeField]
+    private GameObject blockPrefab;
+
+    private void Start()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (randomIndex != i)
+                Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);
+            else
+                continue;
+        }
+    }
+
+
+}
--- Dodge-The-Block/Assets/Scripts/Player.cs
+++ Dodge-The-Block/Assets/Scripts/Player.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float moveSpeed = 15f;
+    [SerializeField]
+    private float mapWidth = 5f;
+
+    private Rigidbody2D rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
     private void FixedUpdate()
     {
-       
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * moveSpeed;
+
+        Vector2 newPosition = rigidbody.position + Vector2.right * x;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
+
+        rigidbody.MovePosition(newPosition);
     }
 }
