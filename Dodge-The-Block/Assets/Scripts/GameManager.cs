1893d356f49324b1efdfc150e47ada142a641087
Jim Beck <jamesbeckjr@gmail.com>
2018-05-05 20:01:15 -0400

    Updated GameManager
    Added slowdown effect when player collides with block

--- Dodge-The-Block/Assets/Scripts/GameManager.cs
+++ Dodge-The-Block/Assets/Scripts/GameManager.cs
@@ -1,12 +1,26 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private float slowDownRate = 10f;
     public void EndGame()
     {
+        StartCoroutine(RestartLevel());
+    }
+
+    private IEnumerator RestartLevel()
+    {
+        Time.timeScale = 1f / slowDownRate;
+        Time.fixedDeltaTime =  Time.fixedDeltaTime / slowDownRate;
+
+        yield return new WaitForSeconds(1f / slowDownRate);
+
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = Time.fixedDeltaTime * slowDownRate;
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
